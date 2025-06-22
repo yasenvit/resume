24404d1b1820878abf7fd2a3fe47f158135c9139
yasenvit <yavitaly74@gmail.com>
2025-06-22 17:38:50 -0400

    Initial commit

--- portfolio_generator.py
+++ portfolio_generator.py
@@ -21,7 +21,7 @@ def get_portfolio_generated():
 
     # Set up Jinja environment
     env = Environment(loader=FileSystemLoader("."), autoescape=True)
-    index_template = env.get_template("resume_template.html")
+    index_template = env.get_template("resume_web_template.html")
     resume_template = env.get_template("resume_pdf_template.html")
 
     # Render the template with the data
@@ -33,7 +33,7 @@ def get_portfolio_generated():
     # resume_output = resume_template.render(name=data["name"], label=data["label"]...)
 
     # Write the output to an HTML file
-    with Path("resume.html").open("w", encoding="utf-8") as f:
+    with Path("resume_web.html").open("w", encoding="utf-8") as f:
         f.write(html_output)
 
     with Path("resume_pdf.html").open("w", encoding="utf-8") as f:
